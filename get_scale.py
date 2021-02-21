4333f413e701518aae5e7ddf0d6b599f2a874eb8
Владимир <74725402+VladimirTigrov@users.noreply.github.com>
2021-02-21 20:56:49 +0300

    Исправлены ошибки

--- get_scale.py
+++ get_scale.py
@@ -3,13 +3,12 @@ def get_scale(response, spn="", longt=0, latt=0):
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
     toponym_coodrinates = toponym["Point"]["pos"]
-    # Левый верхний угол: долгота и широта
-    lower_corner_longitude, lower_corner_lattitude = toponym["boundedBy"]["Envelope"]["lowerCorner"].split(" "
-                                                                                                           )
-    upper_corner_longitude, upper_corner_lattitude = toponym["boundedBy"]["Envelope"]["upperCorner"].split(" "
-                                                                                                           )
-    # Правый нижний угол: долгота и широта
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    lower_corner_longitude, lower_corner_lattitude = toponym["boundedBy"]["Envelope"]["lowerCorner"].split()                                         
+    upper_corner_longitude, upper_corner_lattitude = toponym["boundedBy"]["Envelope"]["upperCorner"].split()
+    lower_corner_longitude, lower_corner_lattitude = float(lower_corner_longitude), float(lower_corner_lattitude)
+    upper_corner_longitude, upper_corner_lattitude = float(upper_corner_longitude), float(upper_corner_lattitude)                                                                                                    
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split()
+    toponym_longitude, toponym_lattitude = float(toponym_longitude), float(toponym_lattitude)
     lower_corner_longitude += longt
     lower_corner_lattitude += latt
     upper_corner_longitude += longt
@@ -19,16 +18,16 @@ def get_scale(response, spn="", longt=0, latt=0):
     delta = "0.005"
     if not spn:
         map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
-            "spn": f'{float(upper_corner_longitude) - float(lower_corner_longitude)},{float(upper_corner_lattitude) - float(lower_corner_lattitude)}',
+            "ll": ",".join([str(toponym_longitude), str(toponym_lattitude)]),
+            "spn": f'{upper_corner_longitude - lower_corner_longitude},{upper_corner_lattitude - lower_corner_lattitude}',
             "l": "map",
-            "pt": f'{",".join([toponym_longitude, toponym_lattitude])},pm2ywl'
+            "pt": f'{",".join([str(toponym_longitude), str(toponym_lattitude)])},pm2ywl'
         }
     else:
         map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
+            "ll": ",".join([str(toponym_longitude), str(toponym_lattitude)]),
             "spn": f'{spn},{spn}',
             "l": "map",
-            "pt": f'{",".join([toponym_longitude, toponym_lattitude])},pm2ywl'
+            "pt": f'{",".join([str(toponym_longitude), str(toponym_lattitude)])},pm2ywl'
         }
     return map_params
